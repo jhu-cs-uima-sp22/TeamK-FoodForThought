3dd503787d562287b6170cced4d6ddde5cd66c03
haydenn <haydenn@jhu.edu>
2022-04-02 15:09:59 -0400

    Adding sample code for user authentication (creating account, signing in/out)

--- app/src/main/java/com/example/foodforthoughtapp/MainActivity.java
+++ app/src/main/java/com/example/foodforthoughtapp/MainActivity.java
@@ -6,9 +6,17 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private UserAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        auth = new UserAuth();
+        test();
+    }
+
+    private void test() {
+        auth.createAccount("haydenn@jhu.edu", "test123");
     }
 }
--- app/src/main/java/com/example/foodforthoughtapp/UserAuth.java
+++ app/src/main/java/com/example/foodforthoughtapp/UserAuth.java
@@ -1,2 +1,65 @@
-package com.example.foodforthoughtapp;public class userAuth {
+package com.example.foodforthoughtapp;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class UserAuth {
+
+    private static String AUTH_TAG = "AUTH";
+    private FirebaseAuth auth;
+
+    public UserAuth() {
+        this.auth = FirebaseAuth.getInstance();
+    }
+
+    // example of creating a user's account -- can inline this code in the create account activity
+    public void createAccount(String email, String password) {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                // code to run upon successful creation of a user's account
+                // TODO: launch activity that appears after the user logs in
+                if (task.isSuccessful()) {
+                    Log.d(AUTH_TAG, "createUserWithEmail:success");
+                    FirebaseUser user = auth.getCurrentUser();
+                    // TODO: update UI and/or launch the next activity
+                } else {
+                    // account creation failed
+                    Log.w(AUTH_TAG, "createUserWithEmail:failure", task.getException());
+                    // TODO: update UI accordingly with error toast -- replace null with actual context
+                    Toast.makeText(null, "Invalid email address and password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void signIn(String email, String password) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d(AUTH_TAG, "signInWithEmail:success");
+                    FirebaseUser user = auth.getCurrentUser();
+                    Log.d(AUTH_TAG, user.toString());
+                } else {
+                    // sign in failed
+                    Log.w(AUTH_TAG, "signInWithEmail:failure", task.getException());
+                }
+            }
+        });
+    }
+
+    public void signOut() {
+        // by default this is only called when the user is logged in
+        auth.signOut();
+        Log.d(AUTH_TAG, "signOut:success");
+    }
 }

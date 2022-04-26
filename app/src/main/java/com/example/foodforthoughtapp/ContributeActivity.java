6abcda6f11f1c85682f336d61281492be2b1f89b
haydenn <haydenn@jhu.edu>
2022-04-26 19:23:39 -0400

    Submitting hours onSubmit for Contribute Activity

--- app/src/main/java/com/example/foodforthoughtapp/ContributeActivity.java
+++ app/src/main/java/com/example/foodforthoughtapp/ContributeActivity.java
@@ -1,6 +1,5 @@
 package com.example.foodforthoughtapp;
 
-import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,12 +7,9 @@ import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
-import android.widget.Spinner;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
@@ -28,8 +24,10 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -54,10 +52,7 @@ public class ContributeActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.contribute_page);
-        //NEW CONTRIBUTE PAGE
         setContentView(R.layout.new_contribute_page);
-//        findViewById(R.id.mainLayout2).setVisibility(View.INVISIBLE);
         //back button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -132,16 +127,15 @@ public class ContributeActivity extends AppCompatActivity {
 
     // submits a user's contribution to the database
     private void submitContribution() {
-        //TODO: get the volunteer hours
-        //List<VolunteerContribution> volunteering = getVolunteerHours();
-        /*if (volunteering != null) {
+        List<VolunteerContribution> volunteering = getVolunteerHours();
+        if (volunteering != null) {
             for (VolunteerContribution contribution : volunteering) {
                 dbref.child("contributions").child(userID).child("volunteerHistory").push().setValue(contribution);
             }
             Log.d("ContributeActivity", "Submitted "
                     + volunteering.size() + " volunteer opportunities for user "
                     + userID);
-        }*/
+        }
 
         Pair<ResourceContribution, Set<String>> donations = getDonation();
         if (donations != null) {
@@ -171,11 +165,16 @@ public class ContributeActivity extends AppCompatActivity {
         if (resources.isEmpty()) {
             return null;
         }
-        // TODO: hardcode date for now
-        String date = "01/04/2020";
-        Pair<ResourceContribution, Set<String>> res = new Pair<>(new ResourceContribution
-                (date, pantryID, resources), donated);
-        return res;
+        try {
+            // TODO: set date to current date for now
+            String date = new SimpleDateFormat("MM/dd/yyyy").parse(new Date().toString())
+                    .toString();
+            return new Pair<>(new ResourceContribution
+                    (date, pantryID, resources), donated);
+        } catch (ParseException ex) {
+            ex.printStackTrace();
+        }
+        return null;
     }
 
     private void submitContributionToPantry(ResourceContribution contribution, Set<String> changed) {
@@ -206,165 +205,12 @@ public class ContributeActivity extends AppCompatActivity {
         });
     }
 
-    /*private List<VolunteerContribution> getVolunteerHours() {
+    private List<VolunteerContribution> getVolunteerHours() {
         List<VolunteerContribution> volunteering = new ArrayList<>();
-        for (String day : pantry.hours.keySet()) {
-            Pair<Spinner, Spinner> times = getTimeSpinners(day);
-            String start = times.first.getSelectedItem().toString();
-            String end = times.second.getSelectedItem().toString();
-            if (!start.equals("None") && !end.equals("None")) {
-                PantryHours hours = new PantryHours(start, end);
-                VolunteerContribution cur = new VolunteerContribution("05/05/2022", pantryID, hours);
-                volunteering.add(cur);
-            }
+        for (int i = 0; i < va.getCount(); i++) {
+            VolDateTime cur = va.getItem(i);
+            volunteering.add(new VolunteerContribution(cur.date, pantryID, cur.time));
         }
         return !volunteering.isEmpty() ? volunteering : null;
     }
-
-    private Pair<Spinner, Spinner> getTimeSpinners(String day) {
-        switch (day) {
-            case "Monday":
-                return new Pair<>(findViewById(R.id.strtSpinnerM), findViewById(R.id.endSpinnerM));
-            case "Tuesday":
-                return new Pair<>(findViewById(R.id.strtSpinnerT), findViewById(R.id.endSpinnerT));
-            case "Wednesday":
-                return new Pair<>(findViewById(R.id.strtSpinnerW), findViewById(R.id.endSpinnerW));
-            case "Thursday":
-                return new Pair<>(findViewById(R.id.strtSpinnerTh), findViewById(R.id.endSpinnerTh));
-            case "Friday":
-                return new Pair<>(findViewById(R.id.strtSpinnerF), findViewById(R.id.endSpinnerF));
-            case "Saturday":
-                return new Pair<>(findViewById(R.id.strtSpinnerSa), findViewById(R.id.endSpinnerSa));
-            case "Sunday":
-                return new Pair<>(findViewById(R.id.strtSpinnerSu), findViewById(R.id.endSpinnerSu));
-            default:
-                return null;
-        }
-    }*/
-
-
-
-    /*private void populateHours(PantryInfo pantry) {
-        Map<String, PantryHours> hours = pantry.hours;
-        LinearLayout days = findViewById(R.id.linearLayout);
-        if (hours.containsKey("Monday")) {
-            Spinner mstartSpinner = (Spinner) findViewById(R.id.strtSpinnerM);
-            Spinner mendSpinner = (Spinner) findViewById(R.id.endSpinnerM);
-            // get array for selecting times based on the hours
-            PantryHours curHours = hours.get("Monday");
-            ((TextView) findViewById(R.id.mTimeTextView)).setText(curHours.toString());
-            List<CharSequence> times = getHoursArray(curHours);
-            setSpinner(mstartSpinner, times);
-            setSpinner(mendSpinner, times);
-        } else {
-            days.removeView(findViewById(R.id.mondayLayout));
-        }
-        if (hours.containsKey("Tuesday")) {
-            Spinner tstartSpinner = (Spinner) findViewById(R.id.strtSpinnerT);
-            Spinner tendSpinner = (Spinner) findViewById(R.id.endSpinnerT);
-            // get array for selecting times based on the hours
-            PantryHours curHours = hours.get("Tuesday");
-            ((TextView) findViewById(R.id.tuesTimeTextView)).setText(curHours.toString());
-            List<CharSequence> times = getHoursArray(curHours);
-            setSpinner(tstartSpinner, times);
-            setSpinner(tendSpinner, times);
-        } else {
-            days.removeView(findViewById(R.id.tuesdayLayout));
-        }
-        if (hours.containsKey("Wednesday")) {
-            Spinner wstartSpinner = (Spinner) findViewById(R.id.strtSpinnerW);
-            Spinner wendSpinner = (Spinner) findViewById(R.id.endSpinnerW);
-            // get array for selecting times based on the hours
-            PantryHours curHours = hours.get("Wednesday");
-            ((TextView) findViewById(R.id.weTimeTextView)).setText(curHours.toString());
-            List<CharSequence> times = getHoursArray(curHours);
-            setSpinner(wstartSpinner, times);
-            setSpinner(wendSpinner, times);
-        } else {
-            days.removeView(findViewById(R.id.wednesdayLayout));
-        }
-        if (hours.containsKey("Thursday")) {
-            Spinner thstartSpinner = (Spinner) findViewById(R.id.strtSpinnerTh);
-            Spinner thendSpinner = (Spinner) findViewById(R.id.endSpinnerTh);
-            // get array for selecting times based on the hours
-            PantryHours curHours = hours.get("Thursday");
-            ((TextView) findViewById(R.id.thursTimeTextView)).setText(curHours.toString());
-            List<CharSequence> times = getHoursArray(curHours);
-            setSpinner(thstartSpinner, times);
-            setSpinner(thendSpinner, times);
-        } else {
-            days.removeView(findViewById(R.id.thursdayLayout));
-        }
-        if (hours.containsKey("Friday")) {
-            Spinner fstartSpinner = (Spinner) findViewById(R.id.strtSpinnerF);
-            Spinner fendSpinner = (Spinner) findViewById(R.id.endSpinnerF);
-            // get array for selecting times based on the hours
-            PantryHours curHours = hours.get("Friday");
-            ((TextView) findViewById(R.id.frTimeTextView)).setText(curHours.toString());
-            List<CharSequence> times = getHoursArray(curHours);
-            setSpinner(fstartSpinner, times);
-            setSpinner(fendSpinner, times);
-        } else {
-            days.removeView(findViewById(R.id.fridayLayout));
-        }
-        if (hours.containsKey("Saturday")) {
-            Spinner sastartSpinner = (Spinner) findViewById(R.id.strtSpinnerSa);
-            Spinner saendSpinner = (Spinner) findViewById(R.id.endSpinnerSa);
-            // get array for selecting times based on the hours
-            PantryHours curHours = hours.get("Saturday");
-            ((TextView) findViewById(R.id.satTimeTextView)).setText(curHours.toString());
-            List<CharSequence> times = getHoursArray(curHours);
-            setSpinner(sastartSpinner, times);
-            setSpinner(saendSpinner, times);
-        } else {
-            days.removeView(findViewById(R.id.saturdayLayout));
-        }
-        if (hours.containsKey("Sunday")) {
-            Spinner sustartSpinner = (Spinner) findViewById(R.id.strtSpinnerSu);
-            Spinner suendSpinner = (Spinner) findViewById(R.id.endSpinnerSu);
-            // get array for selecting times based on the hours
-            PantryHours curHours = hours.get("Sunday");
-            ((TextView) findViewById(R.id.sunTimeTextView)).setText(curHours.toString());
-            List<CharSequence> times = getHoursArray(curHours);
-            setSpinner(sustartSpinner, times);
-            setSpinner(suendSpinner, times);
-        } else {
-            days.removeView(findViewById(R.id.sundayLayout));
-        }
-    }
-
-    @SuppressLint("NewApi")
-    private List<CharSequence> getHoursArray(PantryHours monday) {
-        LocalTime start, end;
-        try {
-            start = LocalTime.parse(monday.startTime);
-            end = LocalTime.parse(monday.endTime);
-            int intervals = (int) start.until(end, ChronoUnit.MINUTES) / 30;
-            List<CharSequence> times = new ArrayList<>(Collections.singletonList("None"));
-            for (int i = 0 ; i < intervals + 1; i++) {
-                times.add(start.plusMinutes(i * 30).toString());
-            }
-            return times;
-        } catch (Exception e) {
-            Log.d("ContributeActivity", e.toString());
-        }
-        return null;
-    }
-
-    public void setSpinner(Spinner spinner, List<CharSequence> times) {
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_spinner_item, times);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-    }
-
-    public boolean onOptionsItemSelected(MenuItem item){
-        Intent myIntent = new Intent(getApplicationContext(), PantryDetail.class);
-        startActivityForResult(myIntent, 0);
-        return true;
-    }*/
-
 }
